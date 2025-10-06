092460b6ca325db06072369296d25334ad8c252d
lozukka <lotsulotsu@gmail.com>
2025-10-06 15:18:47 +0300

    render logic

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const extensionList = document.getElementById("extensions-list");
 
+let extensions = [];
+async function getExtensions() {
+  extensions = await fetchExtensions();
+
+  extensions.forEach((extension) => {
+    const { logo, name, description, isActive } = extension;
+    renderExtension(logo, name, description, isActive);
+  });
+}
+async function fetchExtensions() {
+  try {
+    const response = await fetch("./data.json");
+    if (!response.ok) throw new Error("Failed to load products");
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+function renderExtension(logo, name, description, isActive) {
+  console.log(logo, name, description, isActive);
+  const extensionDiv = document.createElement("div");
+  extensionDiv.classList.add("extension");
+  extensionDiv.innerHTML = `
+          <div class="extension__details">
+            <img src="${logo}" alt="" />
+            <div class="extension__text">
+              <h2 class="extension__name">${name}</h2>
+              <p class="extension__description">
+                ${description}
+              </p>
+            </div>
+          </div>
+          <div class="buttons">
+            <button class="remove-btn">Remove</button>
+            <label class="switch">
+              <input type="checkbox" />
+              <span class="slider round"></span>
+            </label>
+          </div>
+        `;
+  extensionList.appendChild(extensionDiv);
+}
+window.addEventListener("load", getExtensions);
